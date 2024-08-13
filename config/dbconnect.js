2a41255f12fa4c2eebbc0b56b184e98b81cdce51
iHakaze <jonardsaavedra13@gmail.com>
2024-08-13 14:49:51 +0800

    dbconnect.js

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -4,7 +4,8 @@ const dbconfig={
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS ,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectionLimit: 10
   }
   //console.log(dbconfig);
 
