ae94c5338785fe6a1adfc6b4ea9b240681e20d58
iHakaze <jonardsaavedra13@gmail.com>
2025-01-15 22:46:06 +0800

    Update AllowedOrigin.js

--- config/AllowedOrigin.js
+++ config/AllowedOrigin.js
@@ -1,7 +1,8 @@
 const AllowedOrigin =[
     "https://jsaavedra.tech",
      "https://www.jsaavedra.tech/",
-    "http://localhost:5173/"
+    "http://localhost:5173/",
+       "http://localhost/"
  
 ]
 
